52779349af3f255468e4901c39fdcc0d74a5be89
vrnshrm850 <vrnshrm850@gmail.com>
2025-06-13 20:06:46 +0530

    header-banner-fix

---
+++ script.js
@@ -0,0 +1,11 @@
+const nav = document.getElementById('mynav');
+window.onscroll = function () { 
+    if (document.body.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
